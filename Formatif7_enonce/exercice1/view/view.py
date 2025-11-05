c0218c4d1a3bb8815f92e02b4e530e7d03f8cf9b
Gabriel Duguay <74562510+GabrielDuguay@users.noreply.github.com>
2025-11-05 16:53:55 -0500

    Update view.py

--- Formatif7_enonce/exercice1/view/view.py
+++ Formatif7_enonce/exercice1/view/view.py
@@ -1,17 +1,13 @@
-from PyQt6.QtCore import pyqtSignal, Qt
-from PyQt6.QtWidgets import QMainWindow, QListView
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton
 from PyQt6.uic import loadUi
 
 
-class TasksView(QMainWindow):
-    tasksListView:QListView
-    itemClicked = pyqtSignal(str)
+class TasksReadOnlyView(QWidget):
+    label: QLabel
 
     def __init__(self):
         super().__init__()
-        loadUi("view/ui/tasksView.ui", self)
-        self.tasksListView.clicked.connect(self._relay_click)
+        loadUi("view/ui/tasksReadOnlyView.ui", self)
 
-    def _relay_click(self, index):
-        value = index.data(Qt.ItemDataRole.UserRole)
-        self.itemClicked.emit(value)
+    def update_summary(self, total, done):
+        self.label.setText(f"Nombre de taches : {total} dont {done} sont effectuées")
